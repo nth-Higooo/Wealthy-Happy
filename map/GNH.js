b22a046bf84c0ea078cf10d5db474f9671054bfd
Tuan Le <84724694+Tunizrel@users.noreply.github.com>
2024-01-12 01:36:03 +0700

    Update country flag

--- map/GNH.js
+++ map/GNH.js
@@ -1,129 +1,171 @@
- // Read the CSV file
-   d3.csv("data/gnh_data.csv").then(data => {
-    // Use it when the xAxisLabels can't take data
-    //     return{
-    //     country : +d.Name,
-    //     year : +d.Year,
-    //     Dystopia: +d['Dystopia residual'],
-    //    }
-       
-       // Set up the initial year and x-axis variable
-       let selectedYear = 2013;
-       let selectedXAxis = 'dystopia';
+// Read the CSV file
+d3.csv("data/gnh_data.csv").then(data => {
+    
+    const findFlag = (countryName) => {
+        
+    }
+    
+    // Set up the initial year and x-axis variable
+    let selectedYear = 2013;
+    let selectedXAxis = 'Dystopia residual';
+    var xAxisSelector = document.getElementById('xAxisSelector')
+    xAxisSelector.addEventListener('change', (event) => updateScatterPlot());
 
-       // Function to update the scatter plot based on user selections
-       function updateScatterPlot() {
-           selectedYear = parseInt(document.getElementById("yearSelector").value, 10);
-           selectedXAxis = document.getElementById("xAxisSelector").value;
-           drawScatterPlot();
-       }
+    // Function to update the scatter plot based on user selections
+    function updateScatterPlot() {
+        console.log('haha') // DEBUGGING
+        selectedYear = parseInt(document.getElementById("yearSelector").value, 10);
+        selectedXAxis = document.getElementById("xAxisSelector").value;
+        drawScatterPlot();
+    }
 
-       // Initial drawing of the scatter plot
-       drawScatterPlot();
+    // Initial drawing of the scatter plot
+    drawScatterPlot();
 
-       // Function to draw the scatter plot
-       function drawScatterPlot() {
-           // Filter data based on the selected year
-           const filteredData = data.filter(d => +d.Year === selectedYear);
+    // Function to draw the scatter plot
+    function drawScatterPlot() {
+        d3.select('#scatter-plot').selectAll('*').remove();
+        // Filter data based on the selected year
+        const filteredData = data.filter(d => +d.Year === selectedYear);
+        var yearSelector = document.getElementById('yearSelector')
+        yearSelector.addEventListener('change', (event) => updateScatterPlot());
 
-           // Set up the SVG container dimensions
-           const margin = { top: 20, right: 20, bottom: 30, left: 40 };
-           const width = 600 - margin.left - margin.right;
-           const height = 400 - margin.top - margin.bottom;
+        // Set up the SVG container dimensions
+        const margin = { top: 20, right: 20, bottom: 30, left: 40 };
+        const width = 600 - margin.left - margin.right;
+        const height = 400 - margin.top - margin.bottom;
 
-           // Create the SVG container
-           const svg = d3.select("#scatter-plot")
-               .attr("width", width + margin.left + margin.right)
-               .attr("height", height + margin.top + margin.bottom)
-               .append("g")
-               .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+        // Create the SVG container
+        const svg = d3.select("#scatter-plot")
+            .attr("width", width + margin.left + margin.right)
+            .attr("height", height + margin.top + margin.bottom)
+            .append("g")
+            .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-           // Set up scales
-           const xScale = d3.scaleLinear()
-               .domain([d3.min(filteredData, d => +d[selectedXAxis]), d3.max(filteredData, d => +d[selectedXAxis])])
-               .range([0, width]);
+        // Set up scales
+        const xScale = d3.scaleLinear()
+            .domain([d3.min(filteredData, d => +d[selectedXAxis]), d3.max(filteredData, d => +d[selectedXAxis])])
+            .range([0, width]);
 
-           const yScale = d3.scaleLinear()
-               .domain([d3.min(filteredData, d => +d.happiness), d3.max(filteredData, d => +d.happiness)])
-               .range([height, 0]);
+        const yScale = d3.scaleLinear()
+            .domain([0, 10])
+            .range([height, 0]);
 
-           // Add circles to represent data points
-           svg.selectAll("circle")
-               .data(filteredData)
-               .enter().append("circle")
-               .attr("cx", d => xScale(+d[selectedXAxis]))
-               .attr("cy", d => yScale(+d.happiness))
-               .attr("r", 5) // Radius of the circle
-               .on("mouseover", mouseover)
-               .on("mousemove", mousemove)
-               .on("mouseleave", mouseleave);
+        // Add x-axis label based on the selected x-axis variable
+        const xAxisLabels = {
+            'Dystopia residual': 'Dystopia Residual',
+            'Log GDP per capita': 'Log GDP Per Capita',
+            'Social support': 'Social Support',
+            'Healthy life expectancy': 'Healthy Life Expectancy',
+            'Freedom to make life choices': 'Freedom To Make Life Choices',
+            'Generosity': 'Generosity',
+            'Perceptions of corruption': 'Perceptions Of Corruption'
+        };
 
-           // Add x-axis label based on the selected x-axis variable
-           const xAxisLabels = {
-               'Dystopia residual': 'Dystopia Residual',
-               'Log GDP per capita': 'Log GDP Per Capita',
-               'Social support': 'Social Support',
-               'Healthy life expectancy': 'Healthy Life Expectancy',
-               'Freedom to make life choices': 'Freedom To Make Life Choices',
-               'Generosity': 'Generosity',
-               'Perceptions of corruption': 'Perceptions Of Corruption'
-           };
+        // Add x-axis
+        svg.append("g")
+            .attr("transform", "translate(0," + height + ")")
+            .call(d3.axisBottom(xScale))
+            .append("text")
+            .attr("x", width / 2)
+            .attr("y", margin.bottom - 10)
+            .attr("dy", "0.71em")
+            .attr("fill", "#000")
+            .style("fill", "white")
+            .text(xAxisLabels[selectedXAxis]);
 
-           // Add x-axis
-           svg.append("g")
-               .attr("transform", "translate(0," + height + ")")
-               .call(d3.axisBottom(xScale))
-               .append("text")
-               .attr("x", width / 2)
-               .attr("y", margin.bottom - 10)
-               .attr("dy", "0.71em")
-               .attr("fill", "#000")
-               .text(xAxisLabels[selectedXAxis]);
+        // Add y-axis
+        svg.append("g")
+            .call(d3.axisLeft(yScale))
+            .append("text")
+            .attr("transform", "rotate(-90)")
+            .attr("y", -margin.left)
+            .attr("x", -height / 2)
+            .attr("dy", "0.71em")
+            .attr("fill", "#000")
+            .attr("fill","white")
+            .text("Happiness Index");
 
-           // Add y-axis
-           svg.append("g")
-               .call(d3.axisLeft(yScale))
-               .append("text")
-               .attr("transform", "rotate(-90)")
-               .attr("y", -margin.left)
-               .attr("x", -height / 2)
-               .attr("dy", "0.71em")
-               .attr("fill", "#000")
-               .text("Happiness Index");
 
-           // Tooltip
-           var tooltip = d3.select("#scatter-plot")
-               .append("div")
-               .style("opacity", 0)
-               .attr("class", "tooltip")
-               .style("background-color", "white")
-               .style("border", "solid")
-               .style("border-width", "2px")
-               .style("border-radius", "5px")
-               .style("padding", "5px");
 
-           function mouseover(d) {
-               tooltip
-                   .style("opacity", 1);
-               d3.select(this)
-                   .style("stroke", "black")
-                   .style("opacity", 1);
-           }
+svg.append("g")
+    .attr("transform", "translate(0," + height + ")")
+    .call(d3.axisBottom(xScale))
+    .selectAll("*")
+    .style("stroke", "white")
+    .style("fill", "white")
+    .style("stroke-width", "2px")
+    .style("font-size", "12px")
+    .style("font-family", "Arial"); 
 
-           function mousemove(d) {
-               tooltip
-                   .html("Country: " + d.Name)
-                   .style("left", (d3.mouse(this)[0] + 70) + "px")
-                   .style("top", (d3.mouse(this)[1]) + "px");
-           }
 
-           function mouseleave(d) {
-               tooltip
-                   .style("opacity", 0);
-               d3.select(this)
-                   .style("stroke", "none")
-                   .style("opacity", 0.8);
-           }
-       }
-   });
+svg.append("g")
+    .call(d3.axisLeft(yScale))
+    .selectAll("*")
+    .style("stroke", "white")
+    .style("fill", "white")
+    .style("stroke-width", "2px")
+    .style("font-size", "12px")
+    .style("font-family", "Arial"); 
 
+        // // Tooltip
+        // const tooltipscatter = d3.select("#scatter-plot")
+        //     .append("div")
+        //     .attr("class", "tooltip")
+        //     .style("position", "absolute")
+        //     .style("display", "none")
+        //     .style("background-color", "white")
+        //     .style("border", "1px solid black")
+        //     .style("padding", "5px")
+        //     .style("font-size", "12px");
+
+            
+
+        // Add circles to represent data points
+        svg.selectAll("circle")
+            .data(filteredData)
+            .enter().append("circle")
+            .attr("cx", d => xScale(+d[selectedXAxis]))
+            .attr("cy", d => yScale(+d.Happiness))
+            .attr("r", 5) // Radius of the circle
+            .style("fill", "lightgreen")
+            .style("opacity", "0.6")
+
+            
+
+            .on("mouseover",  (e,d) => {
+                //country flag image
+                var flag= document.querySelector("#country-flag");
+                fetch('data/country-flag.json')
+                    .then((response) => response.json())
+                    .then((json) => {
+                        var countryFlag = json
+                        // console.log(countryFlag)
+                        for(const country in countryFlag) {
+                            if(countryFlag[country]['name']==d['Name']) {
+                                console.log(countryFlag[country]['file_url'])
+                                flag.src = countryFlag[country]['file_url']
+                            }
+                        }
+                    });
+
+                //country info
+                var tooltipSection = document.querySelector('#tooltip')
+                var countryName = document.querySelector("#country-name");
+                countryName.textContent = d['Name']
+                
+                var happinesScore = document.querySelector('#happiness-score')
+                happinesScore.textContent='Happiness score: '+d['Happiness']
+            })
+            .on("mouseout", d => {
+                
+            });
+        
+        svg.selectAll("circle")
+            .append('title')
+            .text(d=> d['Name'])
+            // .style("font-size", "20px")
+            // .style("fill", "blue")
+            // .style("text-align", "center");
+    }
+});
